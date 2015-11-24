4ff5a3af8d0966a4f779ecacd1929bab9cf2da9c
BennettIronYard <bennettiy12@gmail.com>
2015-11-24 13:48:31 -0500

    potentially done

--- src/purchases_clojure/core.clj
+++ src/purchases_clojure/core.clj
@@ -1,7 +1,27 @@
 (ns purchases-clojure.core
+  (:require [clojure.string :as str]
+            [clojure.walk :as walk])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main [& args]
+  (println "Type a category")
+  (let [input (read-line)
+        purchses (slurp "purchases.csv")
+        purchses (str/split-lines purchses)
+        purchses (map (fn [line]
+                        (str/split line #","))
+                      purchses)
+        header (first purchses)
+        purchses (rest purchses)
+        purchses (map (fn [line]
+                        (interleave header line))
+                      purchses)
+        purchses (map (fn [line]
+                        (apply hash-map line))
+                      purchses)
+        purchses (walk/keywordize-keys purchses)
+        purchses (filter (fn [line]
+                           (= (:category line) input))
+                         purchses)]
+    (spit "filtered_purchases.edn"
+          (pr-str purchses))))

b082a190c0a2433080119f73e3439ed4d233545a
BennettIronYard <bennettiy12@gmail.com>
2015-11-25 14:39:32 -0500

    Can not access h/html and -main

--- src/purchases_clojure/core.clj
+++ src/purchases_clojure/core.clj
@@ -1,28 +1,49 @@
 (ns purchases-clojure.core
-  (:require [clojure.string :as str]
-            [clojure.walk :as walk]
-            [clojure.pprint :as pprint])
-  (:gen-class))
+(:require [clojure.string :as str]
+          [clojure.walk :as walk]
+          [clojure.pprint :as pprint]
+          [ring.adapter.jetty :as j]
+          [hiccup.core :as h])
+(:gen-class))
+(defn read-purchases []
+(println "Type a category")
+(let [input (read-line)
+      purchses (slurp "purchases.csv")
+      purchses (str/split-lines purchses)
+      purchses (map (fn [line]
+                      (str/split line #","))
+                    purchses)
+      header (first purchses)
+      purchses (rest purchses)
+      purchses (map (fn [line]
+                      (interleave header line))
+                    purchses)
+      purchses (map (fn [line]
+                      (apply hash-map line))
+                    purchses)
+      purchses (walk/keywordize-keys purchses)
+      purchses (filter (fn [line]
+                         (= (:category line) input))
+                       purchses)]
+  #_(spit (format "filtered_%s.edn" input)
+          (with-out-str (pprint/pprint purchses)))
+  purchses))
+
+(defn purchase-html []
+  (let [purchases (read-purchases)]
+    (map (fn [line]
+         [:p
+          (str (:category line)
+               "  "
+               (:date line))])
+       purchases)))
+
+(defn handler [request]
+  {:status  200
+   :headers {"Content-type" "text/html"}
+   :body    (h/html [:html
+                     [:body
+                     (purchase-html)]])})
 
 (defn -main [& args]
-  (println "Type a category")
-  (let [input (read-line)
-        purchses (slurp "purchases.csv")
-        purchses (str/split-lines purchses)
-        purchses (map (fn [line]
-                        (str/split line #","))
-                      purchses)
-        header (first purchses)
-        purchses (rest purchses)
-        purchses (map (fn [line]
-                        (interleave header line))
-                      purchses)
-        purchses (map (fn [line]
-                        (apply hash-map line))
-                      purchses)
-        purchses (walk/keywordize-keys purchses)
-        purchses (filter (fn [line]
-                           (= (:category line) input))
-                         purchses)]
-    (spit (format "filtered_%s.edn" input)
-          (with-out-str (pprint/pprint purchses)))))
+  (j/run-jetty #'handler {:port 3000 :join? false}))
